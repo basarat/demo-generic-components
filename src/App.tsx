2a022b0906c9a5b546989e1b5c8792843edc63b5
basarat <basaratali@gmail.com>
2022-05-10 19:10:00 +1000

    wrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,32 @@
+type TableProps<TItem> = {
+  items: TItem[];
+  renderItem: (item: TItem) => React.ReactNode;
+}
+
+function Table<TItem>(props: TableProps<TItem>) {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+      {props.items.map((x, i) =>
+        <div key={i} style={{ display: 'block' }}>
+          {props.renderItem(x)}
+        </div>
+      )}
+    </div>
+  );
+}
+
+type Data = { name: string, location: string };
+
 function App() {
   return (
-    null
+    <Table<Data>
+      items={[
+        { name: 'Alpha', location: 'Australia' },
+        { name: 'Beta', location: 'America' },
+        { name: 'Gamma', location: 'India' }
+      ]}
+      renderItem={(item) => item.location === 'America' ? <strong>{item.name}</strong> : item.name}
+    />
   );
 }
 
